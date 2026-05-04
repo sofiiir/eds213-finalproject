bd457e80b5ef1218f0a3533bccd5b269177ee8c8
sofiiir <sofiiirodas@gmail.com>
2026-05-04 09:40:30 -0700

    Question for salmon dataset

--- querying-salmon-database.sql
+++ querying-salmon-database.sql
@@ -5,6 +5,7 @@
 SELECT * FROM Salmon LIMIT 5;
 SELECT * FROM Salmon_location LIMIT 5;
 
+-- how many samplings are there for each species based on each projectt type throughout the Alaska salmon dataset from 1922-2017? 
 SELECT species, aslprojecttype, COUNT(*) AS num_by_proj
     FROM Salmon JOIN Salmon_location 
     USING (locationunique)
