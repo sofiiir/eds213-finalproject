f93472fdfe914020293ac8a279d0e29f6a1e4814
sofiiir <sofiiirodas@gmail.com>
2026-05-15 10:56:02 -0700

    Standardise names

--- querying_salmon_database.sql
+++ querying_salmon_database.sql
@@ -5,7 +5,7 @@
 SELECT * FROM Salmon LIMIT 5;
 SELECT * FROM Salmon_location LIMIT 5;
 
--- how many samplings are there for each species based on each projectt type throughout the Alaska salmon dataset from 1922-2017? 
+-- What is the species count for each salmon species based on each project type throughout the Alaska salmon dataset from 1922-2017? 
 SELECT species, aslprojecttype, COUNT(*) AS num_by_proj
     FROM Salmon JOIN Salmon_location 
     USING (locationunique)
